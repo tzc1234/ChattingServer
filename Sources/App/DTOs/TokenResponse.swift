9e7503078fc8173414708687c5f0580f489dc215
tzc1234 <peopli9997@gmail.com>
2024-11-16 09:49:06 +0000

    Change keys to snake_case in TokenResponse

--- Sources/App/DTOs/TokenResponse.swift
+++ Sources/App/DTOs/TokenResponse.swift
@@ -4,4 +4,10 @@ struct TokenResponse: Content {
     let user: UserResponse
     let accessToken: String
     let refreshToken: String
+    
+    enum CodingKeys: String, CodingKey {
+        case user = "user"
+        case accessToken = "access_token"
+        case refreshToken = "refresh_token"
+    }
 }
