2d3b51a7186603cdef71cfa8f9ee6de89d911285
tzc1234 <peopli9997@gmail.com>
2024-11-18 15:43:21 +0000

    Add blockedBy in Contact

--- Sources/App/Migrations/CreateContact.swift
+++ Sources/App/Migrations/CreateContact.swift
@@ -4,11 +4,13 @@ struct CreateContact: AsyncMigration {
     func prepare(on database: Database) async throws {
         try await database.schema("contacts")
             .field("id", .int, .identifier(auto: true))
-            .field("user_id1", .int, .required, .references("users", "user_id", onDelete: .cascade))
-            .field("user_id2", .int, .required, .references("users", "user_id", onDelete: .cascade))
+            .field("user_id1", .int, .required, .references("users", "id", onDelete: .cascade))
+            .field("user_id2", .int, .required, .references("users", "id", onDelete: .cascade))
+            .field("blocked_by", .int, .references("users", "id", onDelete: .cascade))
             .field("created_at", .datetime, .required)
             .unique(on: "user_id1", "user_id2")
             .constraint(.custom("CHECK(user_id1 < user_id2)"))
+            .constraint(.custom("CHECK(blocked_by = user_id1 OR blocked_by = user_id2)"))
             .create()
     }
     
--- Sources/App/Models/Contact.swift
+++ Sources/App/Models/Contact.swift
@@ -13,14 +13,18 @@ final class Contact: Model, @unchecked Sendable {
     @Parent(key: "user_id2")
     var user2: User
     
+    @OptionalParent(key: "blocked_by")
+    var blockedBy: User?
+    
     @Timestamp(key: "created_at", on: .create)
     var createdAt: Date?
     
     init() {}
     
-    init(id: Int? = nil, user1: User, user2: User) {
+    init(id: Int? = nil, userID1: User.IDValue, userID2: User.IDValue, blockedByUserID: User.IDValue? = nil) {
         self.id = id
-        self.user1 = user1
-        self.user2 = user2
+        self.$user1.id = userID1
+        self.$user2.id = userID2
+        self.$blockedBy.id = blockedByUserID
     }
 }
