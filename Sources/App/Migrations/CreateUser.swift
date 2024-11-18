5a4d0b8c1fe22dbc24ac7e57afb599911d6e9977
tzc1234 <peopli9997@gmail.com>
2024-11-18 10:52:10 +0000

    Use datetime instead of date in database schema

--- Sources/App/Migrations/CreateUser.swift
+++ Sources/App/Migrations/CreateUser.swift
@@ -7,8 +7,8 @@ struct CreateUser: AsyncMigration {
             .field("name", .string, .required)
             .field("email", .string, .required)
             .field("password", .string, .required)
-            .field("created_at", .date)
-            .field("updated_at", .date)
+            .field("created_at", .datetime, .required)
+            .field("updated_at", .datetime, .required)
             .unique(on: "email")
             .create()
     }
