5b84d8d3be9b5b19bfbb472ab3a6b2f4eec901de
tzc1234 <peopli9997@gmail.com>
2024-11-19 19:21:35 +0000

    Add pagination parameters to message index api

--- Sources/App/Controllers/MessageController.swift
+++ Sources/App/Controllers/MessageController.swift
@@ -11,24 +11,54 @@ struct MessageController: RouteCollection {
     
     @Sendable
     func index(req: Request) async throws -> MessagesResponse {
-        let payload = try req.auth.require(Payload.self)
-        let userID = payload.userID
-        
         guard let contactIDString = req.parameters.get("contact_id"), let contactID = Int(contactIDString) else {
-            throw Abort(.notFound, reason: "Contact id invalid", identifier: "contact_id_invalid")
+            throw Abort(.badRequest, reason: "Contact id invalid", identifier: "contact_id_invalid")
         }
         
-        guard let contact = try await Contact.query(on: req.db).group(.or, { group in
-            group.filter(\.$user1.$id == userID).filter(\.$user2.$id == userID)
-        })
+        let request = try req.query.decode(MessagesIndexRequest.self)
+        let payload = try req.auth.require(Payload.self)
+        try await checkContactExist(userID: payload.userID, contactID: contactID, db: req.db)
+        
+        let messageQuery = Message.query(on: req.db)
+            .filter(\.$contact.$id == contactID)
+            .limit(request.limit ?? defaultLimit)
+        
+        let messages = try await {
+            if let beforeMessageId = request.beforeMessageID {
+                let tempMessages = try await messageQuery.filter(\.$id < beforeMessageId).sort(.id, .descending).all()
+                return try tempMessages.sorted(by: { try $0.requireID() < $1.requireID() })
+            } else if let afterMessageId = request.afterMessageID {
+                return try await messageQuery.filter(\.$id > afterMessageId).sort(.id, .ascending).all()
+            } else {
+                return try await messageQuery.sort(.id, .ascending).all()
+            }
+        }()
+        return try await messages.toResponse(db: req.db)
+    }
+    
+    private func checkContactExist(userID: Int, contactID: Int, db: Database) async throws {
+        guard try await Contact.query(on: db).group(.or, { group in
+                group.filter(\.$user1.$id == userID).filter(\.$user2.$id == userID)
+            })
             .filter(\.$id == contactID)
-            .with(\.$messages)
-            .first()
+            .count() > 0
         else {
             throw Abort(.notFound, reason: "Contact not found", identifier: "contact_not_found")
         }
-        
-        return try await contact.messages.toResponse(db: req.db)
+    }
+    
+    private var defaultLimit: Int { 20 }
+}
+
+struct MessagesIndexRequest: Content {
+    var beforeMessageID: Int?
+    var afterMessageID: Int?
+    var limit: Int?
+    
+    enum CodingKeys: String, CodingKey {
+        case beforeMessageID = "before_message_id"
+        case afterMessageID = "after_message_id"
+        case limit
     }
 }
 
