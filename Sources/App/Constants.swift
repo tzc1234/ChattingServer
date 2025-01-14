7b1423db6002094ff0abd3b641326ea95aadc279
tzc1234 <peopli9997@gmail.com>
2025-01-14 13:35:57 +0000

    Set access token lifetime to 15 mins

--- Sources/App/Constants.swift
+++ Sources/App/Constants.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 enum Constants {
-    static let ACCESS_TOKEN_LIFETIME: Double = 60 * 1500 // TODO: Set this to 15 mins.
+    static let ACCESS_TOKEN_LIFETIME: Double = 60 * 15 // 15 mins
     static let REFRESH_TOKEN_LIFETIME: Double = 60 * 60 * 24 * 7 // 7 days
     
     static let AVATARS_DIRECTORY: String = "avatars/"
